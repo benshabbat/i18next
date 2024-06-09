810c64c8b174ab852afc03aec592e1babe891d04
benshabbat <benshabbat27@gmail.com>
2024-06-09 18:00:02 +0300

    add to app languageselector

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useTranslation } from "react-i18next"
+import LanguageSelector from "./components/LanguageSelector";
 
 const App = () => {
   const {t}= useTranslation();
   return (
     <div className="container">
+      <LanguageSelector/>
       <h1>{t("greeting")}</h1>
     </div>
   )
--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -2,11 +2,14 @@ const languages = [
   { code: "en", lang: "English" },
   { code: "he", lang: "Hebrew" },
 ];
+const changeLanguage =()=>{
+
+}
 const LanguageSelector = () => {
   return (
     <div className="btn-container">
       {languages.map((lng) => {
-        return <button key={lng.code}>{lng?.lang}</button>;
+        return <button key={lng.code} onClick={changeLanguage}>{lng?.lang}</button>;
       })}
     </div>
   );
