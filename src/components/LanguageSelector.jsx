3c2a28bdb013ee021b4183dc829314c8a27478e8
benshabbat <benshabbat27@gmail.com>
2024-06-09 18:26:21 +0300

    changeLanguage

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,15 +1,26 @@
+import { useTranslation } from "react-i18next";
+
 const languages = [
   { code: "en", lang: "English" },
   { code: "he", lang: "Hebrew" },
 ];
-const changeLanguage =()=>{
-
-}
 const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <div className="btn-container">
       {languages.map((lng) => {
-        return <button key={lng.code} onClick={changeLanguage}>{lng?.lang}</button>;
+        return (
+          <button
+            className={i18n.language === lng.code ? "selected" : ""}
+            key={lng.code}
+            onClick={()=>changeLanguage(lng.code)}
+          >
+            {lng?.lang}
+          </button>
+        );
       })}
     </div>
   );
